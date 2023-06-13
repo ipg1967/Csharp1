18abcaab89a191624b718d991cbf5a254f7490df
ipg <>
2023-06-13 21:04:12 +0300

    новые задачи

---
+++ zad5/Program.cs
@@ -0,0 +1,30 @@
+﻿// See !!! 
+// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+// int number = new Random().Next(10, 100);
+
+Random rand = new Random();
+int randNumber = rand.Next(100, 1000); // правая граница не входит в диапазон]
+System.Console.WriteLine(randNumber);
+System.Console.WriteLine($"{randNumber/100}{randNumber%10}");
+// или 
+int Number = randNumber / 10;
+// int leftNumber = randNumber / 10;
+// int rightNumber = randNumber % 10;
+
+// if (leftNumber > rightNumber)
+// {
+//     System.Console.WriteLine($"Левая цифра больше -> {leftNumber}");
+// }
+// else if (rightNumber >= leftNumber)
+// {
+//     System.Console.WriteLine($"Правая цифра больше -> {rightNumber}");
+// }
+// else
+// {
+//     System.Console.WriteLine("Числа равны");
+// }
