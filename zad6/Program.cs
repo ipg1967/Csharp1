335ecbfbd6cf7ab8d2a314e175b881f88758d67b
ipg <>
2023-06-13 21:16:43 +0300

    новые задачи 2

--- zad6/Program.cs
+++ zad6/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+System.Console.WriteLine("Введите первое число - ");
+int fnumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число - ");
+int snumber = Convert.ToInt32(Console.ReadLine());
+if (fnumber%snumber == 0)
+{
+    System.Console.WriteLine("Число является кратным");
+}
+else
+{
+    System.Console.Write("Число не кратно, остаток ");
+    System.Console.WriteLine(fnumber%snumber );
+}
---
+++ zad7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+// 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+System.Console.WriteLine("Введите число - ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number%7 == 0 && number%23 == 0)
+{
+    System.Console.WriteLine("Число кратно 7 и 23");
+}
+else
+{
+    System.Console.Write("Число не кратно 7 и 23 ");
+}
